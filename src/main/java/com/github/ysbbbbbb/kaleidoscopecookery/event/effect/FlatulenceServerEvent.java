af8dd6407ee17c834f6624c34eda2dced8b6868f
tartaric_acid <baka943@qq.com>
2025-07-22 22:41:14 +0800

    修正一处小错误

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/event/effect/FlatulenceServerEvent.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/event/effect/FlatulenceServerEvent.java
@@ -5,6 +5,7 @@ import com.github.ysbbbbbb.kaleidoscopecookery.init.ModEffects;
 import net.minecraft.core.Holder;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -18,6 +19,18 @@ public class FlatulenceServerEvent {
     public static void onMobEffectEvent(MobEffectEvent.Remove event) {
         LivingEntity entity = event.getEntity();
         Holder<MobEffect> effect = event.getEffect();
+        if (entity instanceof ServerPlayer serverPlayer && effect.is(ModEffects.FLATULENCE)) {
+            // 移除时清除记录
+            if (serverPlayer.hasData(FLATULENCE_EFFECT_STARTING_POSITION)) {
+                serverPlayer.removeData(FLATULENCE_EFFECT_STARTING_POSITION);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onMobEffectEvent(MobEffectEvent.Expired event) {
+        LivingEntity entity = event.getEntity();
+        MobEffectInstance effect = event.getEffectInstance();
         if (entity instanceof ServerPlayer serverPlayer && effect.is(ModEffects.FLATULENCE)) {
             // 移除时清除记录
             if (serverPlayer.hasData(FLATULENCE_EFFECT_STARTING_POSITION)) {
